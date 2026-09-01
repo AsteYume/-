1edd4d793ed71f282e4aea0233e38187633f7d01
Zebi051 <einz.yume@gmail.com>
2026-09-01 09:55:11 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.14 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cs
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cs
@@ -4,26 +4,25 @@ public class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int x = numer1*denom2+numer2*denom1;
         int y = denom1*denom2;
-        
-        int a = x;
-        int b = y;
+        int z = -1;
         if (x<y)
-        {
-            a = y;
-            b = x;
-        }
-        int c = -1;
-        while (true)
-        {
-            c = a % b;
-            if (c == 0)
-            {
-                break;
-            }
-            a = b;
-            b = c;
-        }
-        int[] answer = {x/b,y/b};
+            z = gcd(y,x);
+        else
+            z = gcd(x,y);
+        int[] answer = {x/z,y/z};
         return answer; 
     }
+    
+    public int gcd (int i, int j)
+    {
+        if (j == 0)
+        {
+            return i;
+        }
+        else
+        {
+            return gcd(j,i%j);
+        }
+    }
+    
 }
